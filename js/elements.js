3526906ac3ad493bdb27d1a727935c39959ecc6b
Anthony Lindsay <anthony@netrunner.ie>
2014-04-11 21:41:45 +0100

    Added classes to buttons and confirm dialog markup.

--- js/elements.js
+++ js/elements.js
@@ -1,5 +1,7 @@
-var add_container_button_html = {"html":"<form id='add-container-form'><button id='add-container-button' value='add' type='button'>+ add</button></form>"}
+var add_container_button_html = {"html":"<form id='add-container-form' class='button-form'><button id='add-container-button' value='add' type='button'>+ add</button></form>"}
 
-var global_stop_button_html = {"html":"<form id='global-stop-form'><button id='global-stop-button' value='add' type='button'>x STOP</button></form>"}
+var global_stop_button_html = {"html":"<form id='global-stop-form' class='button-form'><button id='global-stop-button' value='stop' type='button'>x stop</button></form>"}
 
-var container_html = {"html":"<li class='timer-container'><div class='title' contenteditable='true'></div><div class='timer'></div><div class='reset button'>reset</div><div class='delete button'>delete</div></li>"}
+var container_html = {"html":"<li class='timer-container'><div class='delete button'>x</div><div class='title' contenteditable='true'></div><div class='timer'></div><div class='reset button'>reset</div></li>"}
+
+var confirm_dialog_html = {"html":"<div id='dialog-confirm' class='dialog-confirm' title='Delete this timer?'> <span class='ui-icon ui-icon-alert' style='float:left; margin:0 7px 20px 0;'></span>Are you sure?</div>"}
