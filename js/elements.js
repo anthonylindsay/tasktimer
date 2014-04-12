c40704d7b31ac73c4f4f932e49bd38ce851678fd
Anthony Lindsay <anthony@netrunner.ie>
2014-04-12 09:17:22 +0100

    Removed counter reset button and some shortened confirm dialog text.

--- js/elements.js
+++ js/elements.js
@@ -2,6 +2,6 @@ var add_container_button_html = {"html":"<form id='add-container-form' class='bu
 
 var global_stop_button_html = {"html":"<form id='global-stop-form' class='button-form'><button id='global-stop-button' value='stop' type='button'>x stop</button></form>"}
 
-var container_html = {"html":"<li class='timer-container'><div class='delete button'>x</div><div class='title' contenteditable='true'></div><div class='timer'></div><div class='reset button'>reset</div></li>"}
+var container_html = {"html":"<li class='timer-container'><div class='delete button'>x</div><div class='title' contenteditable='true'></div><div class='timer'></div></li>"}
 
 var confirm_dialog_html = {"html":"<div id='dialog-confirm' class='dialog-confirm' title='Delete this timer?'> <span class='ui-icon ui-icon-alert' style='float:left; margin:0 7px 20px 0;'></span>Are you sure?</div>"}
